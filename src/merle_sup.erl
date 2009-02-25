e2a452b6d07589e01a19204d6fccb79f2fc33fd6
Attila Babo <ab@issuu.com>
2009-02-25 14:12:32 +0100

    Add OTP application support.

--- src/merle_sup.erl
+++
@@ -1,8 +0,0 @@
--module(merle_sup).
--author("ab@issuu.com").
--behaviour(supervisor).
--export([init/1]).
-
-init([Host, Port]) ->
-    Server = {merle_service, {gen_server2, start_link, [{local, merle}, merle, [Host, Port], []]}, permanent, 500, worker, [merle]},
-    {ok, {{one_for_one, 15, 5}, [Server]}}.
